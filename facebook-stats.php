c389a4c13702a903fdcc2ac7c85510d47f1c86a6
Alex Gutler <alex11jgt@hotmail.com>
2014-08-10 22:27:15 -0300

    Adicionado o arquivo com o código PHP

---
+++ facebook-stats.php
@@ -0,0 +1,20 @@
+<?php
+    /* 
+        Código simples para contabilidade de compartilhamentos, likes, comments 
+        de uma página do facebook caso queira exibir algumas destas informações em seu site
+
+        Simple code for accounting shares, likes, comments
+        from facebook page if you want to display some of this information on your website
+    */
+    
+    $source_url = "https://www.facebook.com/your-page-here"; //This could be anything URL source including stripslashes($_POST['url'])
+     
+    $url = "http://api.facebook.com/restserver.php?method=links.getStats&urls=".urlencode($source_url);
+    $xml = file_get_contents($url);
+    $xml = simplexml_load_string($xml);
+    $shares = $xml->link_stat->share_count;
+    $likes = $xml->link_stat->like_count;
+    $comments = $xml->link_stat->comment_count;
+    $total = $xml->link_stat->total_count;
+    $max = max($shares,$likes,$comments);
+?>
